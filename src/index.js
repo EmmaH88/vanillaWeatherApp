4ecaa830a3a90efff6531f710413d59204f4c9f0
Emma Hanby <111751257+EmmaH88@users.noreply.github.com>
2023-01-24 13:19:31 +0000

    updated day and time

--- src/index.js
+++ src/index.js
@@ -23,6 +23,33 @@ function displayTemperature(response) {
 
   let wind = document.querySelector("#wind");
   wind.innerHTML = Math.round(response.data.wind.speed);
+
+  let date = document.querySelector("#date");
+  date.innerHTML = formattedDate(response.data.dt * 1000);
+}
+
+function formattedDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 let apiKey = "85bcdd2ad2afb7ce15a8242b1ff12dc1";
